2b3bbc86ad7bd7a60693977613c454ec2033c9f9
jiri.strouhal <jiri.strouhal@gmail.com>
2024-08-12 10:54:24 +0200

    Set up docker compose for external server

--- tests/test_.py
+++ tests/test_.py
@@ -1,17 +1,38 @@
 import unittest
 import sys
+import subprocess
 
-sys.path.append('.')
+sys.path.append(".")
 
 from tests.broker import MQTTBrokerTest
 
 
 class Test_(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.broker = MQTTBrokerTest(start=True)
+        self.module_process = subprocess.Popen(
+            [
+                "python3",
+                "lib/example-module/python_client/run_buttons.py",
+                "--ip",
+                "127.0.0.1",
+                "--port",
+                "1883",
+                "--config",
+                "lib/example-module/python_client/buttons.yaml"
+            ]
+        )
+        self.server_process = subprocess.Popen(["docker", "compose", "up"])
+
     def test_(self):
-        broker = MQTTBrokerTest(start=True)
+        pass
+
+    def tearDown(self):
+        self.module_process.terminate()
+        self.server_process.terminate()
+        self.broker.stop()
 
 
-
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
