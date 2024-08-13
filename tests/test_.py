e2df55629e8c7e2ae58cef06d7d084ae2ac0278c
jiri.strouhal <jiri.strouhal@gmail.com>
2024-08-13 08:21:44 +0200

    Add module gateway

--- tests/test_.py
+++ tests/test_.py
@@ -13,6 +13,7 @@ class Test_Succesfull_Communication_With_Single_Device(unittest.TestCase):
     def setUp(self) -> None:
         self.broker = MQTTBrokerTest(start=True)
         time.sleep(1)
+        subprocess.run(["docker", "compose", "up", "-d"])
         self.module_process = subprocess.Popen(
             [
                 "python3",
@@ -20,19 +21,18 @@ class Test_Succesfull_Communication_With_Single_Device(unittest.TestCase):
                 "--ip",
                 "127.0.0.1",
                 "--port",
-                "1883",
+                "1636",
                 "--config",
                 "lib/example-module/python_client/buttons.yaml"
             ]
         )
-        self.server_process = subprocess.Popen(["docker", "compose", "up"])
 
     def test_succesfull_communication_with_single_device(self):
         time.sleep(10)
 
     def tearDown(self):
         self.module_process.terminate()
-        self.server_process.terminate()
+        subprocess.run(["docker", "compose", "down"])
         self.broker.stop()
 
 
