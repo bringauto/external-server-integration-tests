3c54ddcb2d8f562a0a434bc2e281f7b73778862b
jiri.strouhal <jiri.strouhal@gmail.com>
2024-08-13 09:37:24 +0200

    Fix mismatch in car and company names, make connection sequence to succeed

--- tests/test_.py
+++ tests/test_.py
@@ -2,15 +2,24 @@ import unittest
 import sys
 import subprocess
 import time
+import os
 
 sys.path.append(".")
 
 from tests.broker import MQTTBrokerTest
 
 
+def clear_logs() -> None:
+    if os.path.isfile("./log/external-server/external_server.log"):
+        os.remove("./log/external-server/external_server.log")
+    if os.path.isfile("./log/module-gateway/ModuleGateway.log"):
+        os.remove("./log/module-gateway/ModuleGateway.log")
+
+
 class Test_Succesfull_Communication_With_Single_Device(unittest.TestCase):
 
     def setUp(self) -> None:
+        clear_logs()
         self.broker = MQTTBrokerTest(start=True)
         time.sleep(1)
         subprocess.run(["docker", "compose", "up", "-d"])
@@ -23,12 +32,12 @@ class Test_Succesfull_Communication_With_Single_Device(unittest.TestCase):
                 "--port",
                 "1636",
                 "--config",
-                "lib/example-module/python_client/buttons.yaml"
+                "lib/example-module/python_client/buttons.yaml",
             ]
         )
 
     def test_succesfull_communication_with_single_device(self):
-        time.sleep(10)
+        time.sleep(30)
 
     def tearDown(self):
         self.module_process.terminate()
