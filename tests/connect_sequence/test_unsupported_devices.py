bfa7865393f0219779cf32da6060a16dfae251cc
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-02 09:40:24 +0200

    Test device of unsupported module is ignored during connect sequence

--- tests/connect_sequence/test_unsupported_devices.py
+++ tests/connect_sequence/test_unsupported_devices.py
@@ -32,14 +32,12 @@ from tests._utils.messages import (
 autonomy = device_obj(module_id=1, type=1, role="driving", name="Autonomy", priority=0)
 autonomy_id = device_id(module_id=1, type=1, role="driving", name="Autonomy")
 API_HOST = "http://localhost:8080/v2/protocol"
-unsupported_device = device_obj(module_id=1, type=123456789, role="notdriving", name="UnsupportedDevice", priority=0)
-unsupported_device_id = device_id(module_id=1, type=123456789, role="notdriving", name="UnsupportedDevice")
 
 
 _broker = MQTTBrokerTest()
 
 
-class Test_Connection_Sequence(unittest.TestCase):
+class Test_Unsupported_Device(unittest.TestCase):
 
     def setUp(self) -> None:
         self.broker = _broker
@@ -49,14 +47,24 @@ class Test_Connection_Sequence(unittest.TestCase):
         self.payload = AutonomyStatus().SerializeToString()
 
     def test_only_supported_devices_pass_through_connect_seq_and_have_statuses_forwarded_to_api(self):
+        unsupported_device = device_obj(module_id=1, type=123456789, role="notdriving", name="UnsupportedDevice", priority=0)
         self.ec.post(connect_msg("id", "company_x", "car_a", [autonomy, unsupported_device]), sleep=0.1)
-
         self.ec.post(status("id", DeviceState.CONNECTING, autonomy, 0, self.payload), sleep=0.1)
         self.ec.post(status("id", DeviceState.CONNECTING, unsupported_device, 0, self.payload), sleep=0.1)
-
         self.ec.post(command_response("id", CmdResponseType.OK, 0), sleep=0.1)
         self.ec.post(command_response("id", CmdResponseType.OK, 0), sleep=0.1)
+        s = self.api_client.get_statuses()
+        self.assertEqual(len(s), 1)
+        self.assertEqual(s[0].device_id.module_id, autonomy.module)
+        self.assertEqual(s[0].device_id.name, "Autonomy")
 
+    def test_only_devices_of_supported_modules_pass_through_connect_seq_and_have_statuses_forwarded_to_api(self):
+        unsupp_module_device = device_obj(module_id=123456789, type=1, role="test", name="TestDevice", priority=0)
+        self.ec.post(connect_msg("id", "company_x", "car_a", [autonomy, unsupp_module_device]), sleep=0.1)
+        self.ec.post(status("id", DeviceState.CONNECTING, autonomy, 0, self.payload), sleep=0.1)
+        self.ec.post(status("id", DeviceState.CONNECTING, unsupp_module_device, 0, self.payload), sleep=0.1)
+        self.ec.post(command_response("id", CmdResponseType.OK, 0), sleep=0.1)
+        self.ec.post(command_response("id", CmdResponseType.OK, 0), sleep=0.1)
         s = self.api_client.get_statuses()
         self.assertEqual(len(s), 1)
         self.assertEqual(s[0].device_id.module_id, autonomy.module)
